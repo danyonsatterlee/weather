13d4672b4ac7c708c9d51c20bc96300befee6d5d
danyonsatterlee <danyon.satterlee@gmail.com>
2017-04-24 21:22:49 -0700

    working weather display needs some style

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,69 @@
-var api = 'http://api.openweathermap.org/data/2.5/weather?q='
-var key = '&APPID=6771541381d6281c81e2c11bfc711495='
-var unit = 'imperial'
-
- var input;
+var apikey = 'http://api.openweathermap.org/data/2.5/weather?appid=6771541381d6281c81e2c11bfc711495&units=imperial&q='
 
 
 
-function setup(){
-  if {var button = select('#submit');
-  button.mousePressed(weatherQuery);
-  input = select('#city');
+
+var button = document.getElementById('submit');
+
+button.addEventListener("click", weatherAsk)
+
+function weatherAsk(){
+  // var url = api + input.value() + key + unit;
+var city = document.getElementById('city').value;
+  var url = apikey + city;
+
+
+  $.getJSON( url, function(data) {
+    //data is the JSON string
+
+    var mainTemp = data.main.temp;
+    var high = data.main.temp_max;
+    var low = data.main.temp_min;
+    var cityName = data.name;
+    var description = data.weather.description;
+    var wind =  data.wind.speed;
+    var humidity = data.main.humidity;
+
+    displayTempContainer(mainTemp, high, low);
+    displayGeneralContainer(cityName, description)
+    displayOther(wind, humidity)
+ });
 }
-else {(!city) alert('Please enter a destination.');
+
+function displayTempContainer(mainTemp, high, low) {
+
+  var tempContainer = `
+  <h3>Current Temperature: ${mainTemp}</h3>
+  <h3>High Temperature: ${high}</h3>
+  <h3>Low Temperature: ${low}</h3>
+  `
+
+  $('#temp-container').html(tempContainer)
+
 }
 
-  function weatherQuery(){
-    var url = api + input.value(); + key + unit
-    loadJSON(url,gotData);
+function displayGeneralContainer(cityName) {
+
+  var generalContainer = `
+  <h3>${cityName}</h3>
+  `
+  // <h3>${description}</h3>
 
 
-  }
-  function gotData(data){
-    weather = data;
 
-  }
+  $('#general-container').html(generalContainer)
+
+}
+
+
+function displayOther(wind, humidity) {
+
+  var otherContainer = `
+  <h3>Wind: ${wind}</h3>
+  <h3>Humidity: ${humidity}</h3>
+
+  `
+
+  $('#other-container').html(otherContainer)
+
+}
